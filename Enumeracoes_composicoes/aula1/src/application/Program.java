3600fb4370db96024235ba9baed9dee038e90d15
VictorMoura <mouramvictor@gmail.com>
2022-11-28 16:11:38 -0300

    conclusao exemplo

--- Enumeracoes_composicoes/aula1/src/application/Program.java
+++ Enumeracoes_composicoes/aula1/src/application/Program.java
@@ -1,9 +1,25 @@
 package application;
 
+import java.time.LocalDateTime;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Order order = new Order(1080, LocalDateTime.now(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+		
+		OrderStatus od1 = OrderStatus.DELIVERED;
+		OrderStatus od2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(od1);
+		System.out.println(od2);
+		
+		
 
 	}
 
