cbeadce0e8b733cf0040210f6b873f9834139545
VictorMoura <mouramvictor@gmail.com>
2022-12-02 13:43:50 -0300

    exercicio de formas para calcular areas

---
+++ heranca_polimorfismo/formatoFigura/src/application/Main.java
@@ -0,0 +1,48 @@
+package application;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+import entities.enums.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Shape> list = new ArrayList<>();
+
+        System.out.print("Enter the number of shapes: ");
+        int n = sc.nextInt();
+
+        for(int i = 0; i<n; i++){
+            System.out.println("Shape #"+(i+1)+" data:");
+            System.out.print("Rectangle or Circle (r/c)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Color (BLACK/BLUE/RED): ");
+            Color color = Color.valueOf(sc.next());
+            if(ch == 'r'){
+                System.out.print("Width: ");
+                double width = sc.nextDouble();
+                System.out.print("Height: ");
+                double height = sc.nextDouble();
+                list.add( new Rectangle(color, width, height));
+            } else if (ch == 'c') {
+                System.out.print("Radius: ");
+                double radius = sc.nextDouble();
+                list.add(new Circle(color, radius));
+            }
+        }
+        System.out.println();
+        for(Shape shape: list){
+            System.out.printf("SHAPE AREAS: %.2f%n", shape.area());
+        }
+
+       sc.close();
+    }
+}
