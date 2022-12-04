823982e3c54ba93d9beb09610f36cadd0ab36d33
VictorMoura <mouramvictor@gmail.com>
2022-12-03 21:36:58 -0300

    Exemplo 2 testando chamadas de excecoes

---
+++ excecoes/exemplo_2/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        method1();
+
+        System.out.println("End of program");
+    }
+
+     public static void method1(){
+         System.out.println("***START METHOD 1***");
+         method2();
+         System.out.println("***END METHOD 2***");
+
+     }
+     public static void method2(){
+         System.out.println("***START METHOD 2***");
+         Scanner sc = new Scanner(System.in);
+
+         try {
+             String[] vect = sc.nextLine().split(" ");
+             int position = sc.nextInt();
+             System.out.println(vect[position]);
+         }
+         catch (ArrayIndexOutOfBoundsException e) {
+             System.out.println("Invalid position!");
+             e.printStackTrace();
+         }
+         catch (InputMismatchException e) {
+             System.out.println("Input error");
+         }
+         System.out.println("***END METHOD 2***");
+         sc.close();
+
+     }
+}
