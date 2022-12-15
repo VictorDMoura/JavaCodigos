8b285f0efbac332fba323c7b82e23efc1f22cd98
VictorMoura <mouramvictor@gmail.com>
2022-12-15 13:08:15 -0300

    lendo arquivo csv

--- files/fileProposto/src/application/Main.java
+++ files/fileProposto/src/application/Main.java
@@ -1,7 +1,27 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String strPath = "data.csv";
+
+        try(BufferedReader bf = new BufferedReader(new FileReader(strPath))){
+
+            String line = bf.readLine();
+            while (line != null){
+                System.out.println(line);
+                line = bf.readLine();
+            }
+
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
     }
 }
