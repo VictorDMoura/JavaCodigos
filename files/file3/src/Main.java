7dea6ba429f3eddb054bdb2b47b7feb1d698fdcb
VictorMoura <mouramvictor@gmail.com>
2022-12-15 12:04:55 -0300

    lendo arquivos com mais eficiencia

--- files/file3/src/Main.java
+++ files/file3/src/Main.java
@@ -5,11 +5,9 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         String path = "/home/victordmoura/cursoJava/codigos/files/in.txt";
-        BufferedReader br = null;
-        FileReader fr = null;
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path)) ){
+            
             String line = br.readLine();
             while (line != null) {
                 System.out.println(line);
@@ -17,15 +15,7 @@ public class Main {
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally {
-            try {
-                if (br != null)
-                    br.close();
-                if (fr != null)
-                    fr.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+
     }
 }
