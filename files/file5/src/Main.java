fe685ca646ca73f90a000371e0dc58951019f506
VictorMoura <mouramvictor@gmail.com>
2022-12-15 12:48:17 -0300

    lendo pastas e arquivos e criando pastas

---
+++ files/file5/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a folder path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        File[] folders = path.listFiles(File::isDirectory);
+        System.out.println("Folders: ");
+        for(File folder: folders){
+            System.out.println(folder);
+        }
+        System.out.println("Files:");
+        File[] files = path.listFiles(File::isFile);
+        for(File file: files){
+            System.out.println(file);
+        }
+
+        boolean success = new File(strPath + "/newFile").mkdir();
+        System.out.println("Directory created successfully " + success);
+
+        sc.close();
+    }
+}
